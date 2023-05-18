6ea4c86a51c5a3994d60186f7bd7ac5673fa216c
Ori Korngut <ori.korngut@gmail.com>
2023-05-18 23:13:03 +0300

    LastVerision

--- Control/Imports & Sets.py
+++ Control/Imports & Sets.py
@@ -1,3 +1,17 @@
-def BoardMove(height):
-    height = height * -1
-    motorc.run_to_degrees_counted(height ,90)
+#----------------------------------------------- SoftWare Updates / Imports --------------------------------------------------
+from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
+from spike.control import wait_for_seconds, wait_until, Timer
+from math import *
+import time
+#----------------------------------------------- Motor / Variable Declaration--------------------------------------------------
+hub = PrimeHub()
+motora = Motor('A')
+motorb = Motor('B')
+motorc = Motor('C')
+motord = Motor('D')
+pairAB = MotorPair('A','B')
+hub.motion_sensor.reset_yaw_angle()
+motorc.set_degrees_counted(0)
+perror = 0
+I = 0
+x = hub.motion_sensor.get_yaw_angle()
