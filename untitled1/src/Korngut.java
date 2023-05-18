ede1df353fe080ebce070248e55e0e0e0b4e25f2
Ori Korngut <ori.korngut@gmail.com>
2023-05-18 18:17:20 +0300

    The code from the competition

--- untitled1/src/Korngut.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-    public class Korngut {
-        public static void main(String[] args) {
-        Scanner input - new Scanner (System.in);
-
-        int a = input nextInt;
-        }
-
-}
